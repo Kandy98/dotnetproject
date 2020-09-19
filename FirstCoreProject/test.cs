8e8e775f0baf54ec66d1c5c636854a8272a48e7a
Kandarp Verma <kanverma@publicisgroupe.net>
2020-09-19 19:16:36 +0530

    updated

--- FirstCoreProject/test.cs
+++ FirstCoreProject/test.cs
@@ -6,9 +6,15 @@ namespace FirstCoreProject {
     public class test{
 
         [Fact]
-        public void testName() {
+        public void test1() {
             var ob = new Addition();
             Assert.Equal("less than 10", ob.display(2,3));
+        }
+
+        [Fact]
+        public void test2() {
+            var ob = new Addition();
+            Assert.Equal("greater than 10", ob.display(6,6));
         }
     }
 }
